3fe28dc62de17cee8d5fc49043cd209262a1373d
nadav-av <avr.nadav@gmail.com>
2022-07-24 12:39:36 +0300

    adds functionalitiy for logging in

--- client/src/Components/LoginForm/LoginForm.jsx
+++ client/src/Components/LoginForm/LoginForm.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./loginForm.css";
+import userClient from "../../Services/userClient";
 
 const LoginForm = () => {
+  const [username, setUsername] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await userClient.login(
+      e.target.username.value,
+      e.target.password.value
+    );
+    if (res) {
+      setUsername(res);
+    }
+  };
+
   return (
     <div className="center">
       <h1>Login</h1>
@@ -16,8 +30,11 @@ const LoginForm = () => {
           <span></span>
           <label>Password</label>
         </div>
-        <input type="submit" value="Login" />
+        <input type="submit" value="Login" onClick={handleSubmit} />
       </form>
+      <div>
+        <h1>{username}</h1>
+      </div>
     </div>
   );
 };
