41c201c2e302ec8db835da2f2779fe1ce43f9282
Shalev Sharf <81631445+shalevsh@users.noreply.github.com>
2022-07-25 17:53:50 +0300

    visual map submit

--- client/src/Components/mapForm/MapForm.jsx
+++ client/src/Components/mapForm/MapForm.jsx
@@ -3,27 +3,19 @@ import "./mapForm.css";
 
 
 const MapForm = () => {
-  const [availible, setAvailible] = useState(true);
-
-
-  const func = async (e) => {
-
+  const [officeId, setofficeId] = useState(1);
+  const [officePosition, setOfficePosition] = useState(["c1","c2","c3","c4","c5","c6","c7","c8","o1","o2","o3","o4","o5","o6"]);
+  const getBooking = async (officeId,bookingPlace) => {  
+    console.log(officeId,bookingPlace)  
   };
 
   return (
     <div className="map-center">
       <h1>map</h1>
       <div id="mapBackGround"> 
-      <button class="btn" id="c1">c1</button>
-      <button class="btn"id="c2">c2</button>
-      <button class="btn"id="c3">c3</button>
-      <button class="btn"id="c4">c4</button>
-      <button class="btn"id="c5">c5</button>
-      <button class="btn"id="c6">c6</button>
-      <button class="btn"id="c7">c7</button>
-      <button class="btn"id="c8">c8</button>
-  
-
+      {officePosition.map((element) =>       
+         <button className="btn" id={element} key={element} onClick={()=> getBooking(officeId,element)}>{element}</button>
+      )}
       </div>
       </div>
 
